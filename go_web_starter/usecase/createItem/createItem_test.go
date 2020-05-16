636c7bc64d1a8c3423fe9c752d60ac5b07e950e6
Amith Nair <amith@alefeducation.com>
2020-05-16 19:35:28 +0400

    create test scenario using ginko test runner

--- go_web_starter/usecase/createItem/createItem_test.go
+++ go_web_starter/usecase/createItem/createItem_test.go
@@ -1,12 +1,8 @@
 package createItem_test
 
 import (
-	"github.com/amithnair91/go_web_stack/go_web_starter/domain"
-	"github.com/amithnair91/go_web_stack/go_web_starter/usecase/createItem"
-	mockstorage "github.com/amithnair91/go_web_stack/go_web_starter/usecase/mocks"
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Create Item", func() {
@@ -20,44 +16,70 @@ var _ = Describe("Create Item", func() {
 		mockCtrl.Finish()
 	})
 
-	Context("when Item Does Not Exist", func() {
+	Context("With Valid Input", func() {
 
-		var capturedItem domain.Item
+		Context("And Item Does Not exists", func() {
 
-		//var mockItemStorage storage.ItemStorage
-		//BeforeEach(func() {
-		//	mockCtrl = gomock.NewController(GinkgoT())
-		//	mockItemStorage := mock_storage.NewMockItemStorage(mockCtrl)
-		//	mockItemStorage.
-		//		EXPECT().
-		//		Save(gomock.Any()).
-		//		Do(func(arg domain.Item) {
-		//			capturedItem = arg
-		//		}).MaxTimes(1)
-		//})
+			It("should save item to storage", func() {
 
-		It("should create Item", func() {
-			mockCtrl = gomock.NewController(GinkgoT())
-			mockItemStorage := mockstorage.NewMockItemStorage(mockCtrl)
-			mockItemStorage.
-				EXPECT().
-				Save(gomock.Any()).
-				Do(func(arg domain.Item) {
-					capturedItem = arg
-				}).MaxTimes(1)
-
-			itemName := "bag"
-			input := createItem.Input{Name: itemName}
-			usecase := createItem.Usecase{ItemStorage: mockItemStorage}
-			usecase.Execute(input)
-
-			Expect(capturedItem.Name).To(Equal(itemName))
-		})
-	})
-
-	Context("when Item Exists", func() {
-		It("should return error with message item already exists", func() {
+			})
 
 		})
+
+		Context("And Item exists", func() {
+			It("should return error with message Item already exists", func() {})
+
+			It("should not save item to storage", func() {})
+		})
+
 	})
+
+	Context("With InValid Input", func() {
+
+		It("should return error with message invalid input", func() {})
+
+	})
+
+
+
+	//Context("when Item Does Not Exist", func() {
+	//
+	//	var capturedItem domain.Item
+	//
+	//	//var mockItemStorage storage.ItemStorage
+	//	//BeforeEach(func() {
+	//	//	mockCtrl = gomock.NewController(GinkgoT())
+	//	//	mockItemStorage := mock_storage.NewMockItemStorage(mockCtrl)
+	//	//	mockItemStorage.
+	//	//		EXPECT().
+	//	//		Save(gomock.Any()).
+	//	//		Do(func(arg domain.Item) {
+	//	//			capturedItem = arg
+	//	//		}).MaxTimes(1)
+	//	//})
+	//
+	//	It("should create Item", func() {
+	//		mockCtrl = gomock.NewController(GinkgoT())
+	//		mockItemStorage := mockstorage.NewMockItemStorage(mockCtrl)
+	//		mockItemStorage.
+	//			EXPECT().
+	//			Save(gomock.Any()).
+	//			Do(func(arg domain.Item) {
+	//				capturedItem = arg
+	//			}).MaxTimes(1)
+	//
+	//		itemName := "bag"
+	//		input := createItem.Input{Name: itemName}
+	//		usecase := createItem.Usecase{ItemStorage: mockItemStorage}
+	//		usecase.Execute(input)
+	//
+	//		Expect(capturedItem.Name).To(Equal(itemName))
+	//	})
+	//})
+	//
+	//Context("when Item Exists", func() {
+	//	It("should return error with message item already exists", func() {
+	//
+	//	})
+	//})
 })
