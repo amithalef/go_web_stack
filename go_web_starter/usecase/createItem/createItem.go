46ba2e3ae48de68321a3e21dbae1bea350063adb
Amith Nair <amith@alefeducation.com>
2020-05-17 17:01:36 +0400

    refactor

--- go_web_starter/usecase/createItem/createItem.go
+++ go_web_starter/usecase/createItem/createItem.go
@@ -6,9 +6,6 @@ import (
 	"github.com/amithnair91/go_web_stack/go_web_starter/usecase/storage"
 )
 
-type Usecase struct {
-	ItemStorage storage.ItemStorage
-}
 
 type Input struct {
 	Name string
@@ -19,11 +16,14 @@ func (i *Input) ToItem() (*domain.Item, error) {
 	return domain.NewItem(i.Name)
 }
 
+type Usecase struct {
+	ItemStorage storage.ItemStorage
+}
+
 func (u *Usecase) Execute(input Input) error {
 	if len(input.Name) < 1 {
 		return errors.New("name cannot be empty")
 	}
-
 	exists := u.ItemStorage.Exists(input.Id)
 	if exists {
 		return errors.New("Item Already exists")
